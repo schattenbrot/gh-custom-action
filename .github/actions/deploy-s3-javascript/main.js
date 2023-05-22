07496716c0a37cb1947f6a61aefd6ff18f4d9651
Schattenbrot <aaron.machill@web.de>
2023-05-22 21:14:56 +0200

    add custom action values

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // Upload files
+  const s3uri = `s3:://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`);
+
   core.notice('Print stuff!!!');
 }
 
